4f32f3b129193590a496ead2db3212958b1faff5
Fanta Coulibaly <fantacoulibaly101@gmail.com>
2023-06-14 17:47:32 +0000

    m

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.css']
+  styleUrls: ['./accueil.component.css'],
+  
 })
 export class AccueilComponent {
-
+ 
 }
